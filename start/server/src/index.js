f501e1e6537acc87ca7a30e5edd17c06c9521d07
nicolas-bofi <nicolas.bofi@etu.umontpellier.fr>
2022-04-21 09:47:48 +0200

    Write querry resolvers finished

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
-const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./schema");
-const { createStore } = require("./utils");
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers'); // highlight-line
 
-const LaunchAPI = require("./datasources/launch");
-const UserAPI = require("./datasources/user");
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers, // highlight-line
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store }),
-  }),
+    userAPI: new UserAPI({ store })
+  })
 });
 
 server.listen().then(() => {
