1d854355cd9984fa7b4b7a53b05017b3d8dba9d2
nicolas-bofi <nicolas.bofi@etu.umontpellier.fr>
2022-04-21 11:41:52 +0200

    Fetch data with queries finished

--- start/client/src/index.jsx
+++ start/client/src/index.jsx
@@ -1,7 +1,22 @@
-import { ApolloClient } from "@apollo/client";
-import { cache } from "./cache";
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { cache } from './cache';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
+// Initialize ApolloClient
 const client = new ApolloClient({
   cache,
-  uri: "http://localhost:4000/graphql"
+  uri: 'http://localhost:4000/graphql',
 });
+
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
